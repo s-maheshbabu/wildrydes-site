e20ca4f5a0a7182883b4f8d509292df06faeff85
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2022-07-25 23:49:26 -0700

    asdfasd

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_eXCo7fio0', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6qfe5tc32gh8sl7hef4hoji1v9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_UDa9Z7voZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7joan039msuapdun083dm9dlu1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://xjd4ro2sdg.execute-api.us-east-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://uix2wyrme4.execute-api.us-east-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
